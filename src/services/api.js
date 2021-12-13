bc1b8ea2e6a08af760984cc40218ea5dae788f62
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-12-13 12:43:02 +0200

    Added api

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '24447293-d3f0d6bbd906e1eb5560775ff';
+
+const fetchImages = ({ searchQuery = '', currentPage = 1 }) => {
+  const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${currentPage}&per_page=12&key=${API_KEY}`;
+
+  return axios.get(url);
+};
+
+export default fetchImages;
